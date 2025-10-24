47cf843fb6591df20d9e6dd350018872a6f7cddc
unofficiallybhav <khuranabhavyy@gmail.com>
2025-10-24 19:54:27 +0530

    restructured pipeline

--- src/main.py
+++ src/main.py
@@ -1,34 +1,30 @@
 from preprocessing import load_unsw_data, preprocess_data
-from feature import visualize_pca
-from models import train_random_forest, train_xgboost
-from anomaly import isolation_forest, one_class_svm
+from models import train_random_forest, train_xgboost,train_lightgbm,train_logistic_regression
+from anomaly import run_isolation_forest, run_one_class_svm
 from explainability import explain_with_shap
-from utils import save_model,save_confusion_matrix
+from selection import apply_pca
 
 def main():
     print("[1] Loading and Preprocessing Data...")
     df = load_unsw_data("../data/")
     X_train, X_test, y_train, y_test = preprocess_data(df)
 
-    print("[2] Visualizing PCA...")
-    visualize_pca(X_train, y_train)
-
-    print("[3] Training Supervised Models...")
+    print("[2] Training Supervised Models...")
     rf_model = train_random_forest(X_train, y_train, X_test, y_test)
     xgb_model = train_xgboost(X_train, y_train, X_test, y_test)
+    lgb_model = train_lightgbm(X_train, y_train, X_test, y_test)
+    logistic_regression_model=train_logistic_regression(X_train, y_train, X_test, y_test)
 
-    print("[4] Running Unsupervised Anomaly Detection...")
-    isolation_forest(X_test, y_test)
-    one_class_svm(X_test, y_test)
+    print("[3] Visualizing PCA...")
+    X_train,X_test,_=apply_pca(X_train, y_train,n_components=15)
 
-    print("[5] Model Explainability (SHAP)...")
+    print("\n[4] Running Unsupervised Anomaly Detection Models...")
+    iso_model, iso_auc = run_isolation_forest(X_train, X_test, y_test)
+    ocsvm_model, ocsvm_auc = run_one_class_svm(X_train, X_test, y_test)
+
+    print("\n[5] Model Explainability (SHAP)...")
     explain_with_shap(rf_model, X_test)
 
-    print("[6] Saving Outputs")
-    save_model(rf_model, "random_forest.pkl")
-    save_confusion_matrix(y_test, rf_model.predict(X_test), "RandomForest",'confusion_matrix_rf')
-    save_model(xgb_model, "xgboost.pkl")
-    save_confusion_matrix(y_test, xgb_model.predict(X_test), "XGBoost",'confusion_matrix_xgb')
     
 if __name__ == "__main__":
     main()
